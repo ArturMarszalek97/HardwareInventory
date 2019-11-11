f917eea21823e7751683ca5c2b9d9a2421643341
Artur <amarszalek97@wp.pl>
2019-11-11 18:26:58 +0100

    Added new projects

---
+++ HardwareInventoryService.Modules.Authorization.Service/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.Modules.Authorization.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ HardwareInventoryService.Modules.Cache.Logic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.Modules.Cache.Logic
+{
+    public class Class1
+    {
+    }
+}
