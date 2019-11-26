ce229490cd4d2d5acc697e997690abd5c81b9dd5
Artur <amarszalek97@wp.pl>
2019-11-26 23:52:56 +0100

    Added AuthorizationService in ServiceReferences

---
+++ HardwareInventoryService.ServicesReferences/Contracts/IAuthorizationService.cs
@@ -0,0 +1,13 @@
+﻿using HardwareInventoryService.ServicesReferences.AuthorizationServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.ServicesReferences.Contracts
+{
+    public interface IAuthorizationService : IAuthorizationWCFContract
+    {
+    }
+}
---
+++ HardwareInventoryService.ServicesReferences/Services/AuthorizationService.cs
@@ -0,0 +1,22 @@
+﻿using HardwareInventoryService.ServicesReferences.AuthorizationServiceReference;
+using HardwareInventoryService.ServicesReferences.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.ServicesReferences.Services
+{
+    public class AuthorizationService : AuthorizationWCFContractClient, IAuthorizationService
+    {
+        private readonly ILoggerService _loggerService;
+
+        public AuthorizationService(ILoggerService loggerService)
+        {
+            this._loggerService = loggerService;
+
+            this.ChannelFactory.Open();
+        }
+    }
+}
