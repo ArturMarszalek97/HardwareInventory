7ba43da94a59b9818eb2f747996f2a03bc3df6a9
Artur <amarszalek97@wp.pl>
2019-11-28 01:27:41 +0100

    connected cache service to ServicesReferences project

---
+++ HardwareInventoryService.ServicesReferences/Contracts/ICacheService.cs
@@ -0,0 +1,13 @@
+﻿using HardwareInventoryService.ServicesReferences.CacheServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.ServicesReferences.Contracts
+{
+    public interface ICacheService : ICacheWCFContract
+    {
+    }
+}
---
+++ HardwareInventoryService.ServicesReferences/Services/CacheService.cs
@@ -0,0 +1,22 @@
+﻿using HardwareInventoryService.ServicesReferences.CacheServiceReference;
+using HardwareInventoryService.ServicesReferences.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.ServicesReferences.Services
+{
+    public class CacheService : CacheWCFContractClient, ICacheService
+    {
+        private readonly ILoggerService _loggerService;
+
+        public CacheService(ILoggerService loggerService)
+        {
+            this._loggerService = loggerService;
+
+            this.ChannelFactory.Open();
+        }
+    }
+}
