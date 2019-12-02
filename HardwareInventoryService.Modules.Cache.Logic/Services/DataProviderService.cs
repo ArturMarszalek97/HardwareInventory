d8e071539dbb7ec3695ace47b9f57b6dd7772503
Artur <amarszalek97@wp.pl>
2019-12-02 16:23:39 +0100

    created dataproviderservice class

---
+++ HardwareInventoryService.Modules.Cache.Logic/Services/DataProviderService.cs
@@ -0,0 +1,26 @@
+﻿using HardwareInventoryService.DAO;
+using HardwareInventoryService.Modules.Cache.Logic.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.Modules.Cache.Logic.Services
+{
+    public class DataProviderService
+    {
+        HardwareInventoryEntities context = new HardwareInventoryEntities();
+
+        ISessionRepository sessionRepository;
+
+        public async Task GetUsers()
+        {
+            //return await Task.Run(() =>
+            //{
+            //    var users = this.context.Users.ToList();
+            //    //users.ForEach(x => this.sessionRepository.AddSaved(x));
+            //});
+        }
+    }
+}
