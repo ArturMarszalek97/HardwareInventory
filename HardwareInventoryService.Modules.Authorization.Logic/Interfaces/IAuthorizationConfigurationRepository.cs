45150e8cca4a2dc08aab35c3c5b26e349be000f4
Artur <amarszalek97@wp.pl>
2019-12-05 01:32:34 +0100

    Added AuthorizationConfigurationRepository

---
+++ HardwareInventoryService.Modules.Authorization.Logic/Interfaces/IAuthorizationConfigurationRepository.cs
@@ -0,0 +1,16 @@
+﻿using HardwareInventoryService.Models.Models.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.Modules.Authorization.Logic.Interfaces
+{
+    public interface IAuthorizationConfigurationRepository
+    {
+        AuthorizationModuleConfiguration Get();
+
+        void Set(AuthorizationModuleConfiguration config);
+    }
+}
---
+++ HardwareInventoryService.Modules.Authorization.Logic/Logic/AuthorizationConfigurationRepository.cs
@@ -0,0 +1,41 @@
+﻿using HardwareInventoryService.Models.Models.Authorization;
+using HardwareInventoryService.Modules.Authorization.Logic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.Modules.Authorization.Logic.Logic
+{
+    public class AuthorizationConfigurationRepository : IAuthorizationConfigurationRepository
+    {
+        private AuthorizationModuleConfiguration _configuration;
+
+        private readonly object _configurationLock = new object();
+
+        public AuthorizationConfigurationRepository()
+        {
+            this._configuration = new AuthorizationModuleConfiguration();
+            this._configuration.AccountBlockTime = 5;
+            this._configuration.JWTSecretKey = "klucz";
+            this._configuration.MaximumLoginFailures = 3;
+        }
+
+        private AuthorizationModuleConfiguration AuthorizationModuleConfiguration
+        {
+            get { lock (this._configurationLock) { return this._configuration; } }
+            set { lock (this._configurationLock) { this._configuration = value; } }
+        }
+
+        public AuthorizationModuleConfiguration Get()
+        {
+            return this.AuthorizationModuleConfiguration;
+        }
+
+        public void Set(AuthorizationModuleConfiguration config)
+        {
+            this.AuthorizationModuleConfiguration = config;
+        }
+    }
+}
