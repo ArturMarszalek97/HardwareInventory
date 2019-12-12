9c9687aaa05c168342d3a764fdadd77893477883
Artur <amarszalek97@wp.pl>
2019-12-12 01:09:09 +0100

    Added new models

---
+++ HardwareInventoryService.Models/Models/Enums/ItemCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.Models.Models.Enums
+{
+    public enum ItemCategory
+    {
+        Elektronika,
+        Odzież,
+        Obuwie,
+        Jedzenie,
+        Zdrowie,
+        Kosmetyki,
+        Dom,
+        Akcesoria,
+        Sporte,
+        Biuro,
+        Motoryzacja,
+        Zabawki,
+        Inne
+    }
+}
---
+++ HardwareInventoryService.Models/Models/Item.cs
@@ -0,0 +1,28 @@
+﻿using HardwareInventoryService.Models.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryService.Models.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public string ItemName { get; set; }
+
+        public float Price { get; set; }
+
+        public DateTime DateOfPurchase { get; set; }
+
+        public string Shop { get; set; }
+
+        public ItemCategory Category { get; set; }
+
+        public List<Note> Notes { get; set; }
+
+        public int Warranty { get; set; }
+    }
+}
